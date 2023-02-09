d9962fc27d939398e47d46badf830e41facfbf99
gb-software <gkbbeck@tutanota.com>
2023-02-08 21:02:27 -0700

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+#![no_main]
+#![no_std]
+
+use panic_halt as _;
+
+use cortex_m_rt::entry;
+use embedded_hal::{blocking::delay::DelayMs, digital::v2::OutputPin};
+use microbit::{board::Board, hal::timer::Timer};
+
+#[entry]
+fn main() -> ! {
+    let mut board = Board::take().unwrap();
+
+    let mut timer = Timer::new(board.TIMER0);
+
+    let _ = board.display_pins.col1.set_low();
+    let mut row1 = board.display_pins.row1;
+
+    loop {
+        let _ = row1.set_low();
+        timer.delay_ms(1000u16);
+        let _ = row1.set_high();
+        timer.delay_ms(1000u16);
+    }
+}
